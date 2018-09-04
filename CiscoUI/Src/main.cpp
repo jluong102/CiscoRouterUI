dc320511c31347a5c0ce9c060be2ce17e49a0f81
jluong102 <36459518+jluong102@users.noreply.github.com>
2018-09-04 16:26:39 -0400

    Update main.cpp

--- CiscoUI/Src/main.cpp
+++ CiscoUI/Src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QPixmap pixmap("D:\\Other\\QT\\Projects\\build-CiscoRouter-Desktop_Qt_5_9_5_MinGW_32bit-Release\\splash.png");
+    QPixmap pixmap(".\\splash.png");
     QSplashScreen splash(pixmap);
     splash.show();
     a.thread()->sleep(3);
